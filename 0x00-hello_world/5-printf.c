8ffa5a4dba6ada2ee3cbdd278866ef4c6dcf553e
solojobbs <solojobbs@gmail.com>
2023-09-07 08:00:07 -0700

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/*
- * This program 
- * makes use of
- *  the printf 
- *  function to 
- *  output
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
-/*
- * Displays the size 
- * of
- * the various
- * data 
- * types
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-	long int x;
-	long long int y;
-	char m;
-	float z;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(m));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(n));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
