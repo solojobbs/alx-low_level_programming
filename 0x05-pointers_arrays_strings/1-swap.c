ded242ffa300add10ad20c1fa81bbf70c72da77a
solojobbs <solojobbs@gmail.com>
2023-09-20 04:34:09 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,7 @@
 /**
  * swap_int swaps the values of two integers
  *
- * @x: first entry 
+ * @x: first entry
  * @y: second entry
  *
  *
@@ -12,6 +12,7 @@
 void swap_int(int *x, int  *y)
 {
 	int temp;
+
 	temp = *x;
 	*x = *y;
 	*y = temp;
