dabcf176a4d19814d2692deb2bbf2f3608a37c8f
solojobbs <solojobbs@gmail.com>
2023-09-28 09:24:05 -0700

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,10 +11,10 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-	{	
+	{
 		return;
 	}
-	s++;	
+	s++;
 	_print_rev_recursion(s);
 	s--;
 	_putchar(*s);
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ *
+ * @a: the string of which lenghth to be found
+ *
+ * the function returns the length of the string
+ *
+ */
+
+int  _strlen_recursion(char *a)
+{
+	if (*a == '\0')
+	{
+		return (0);
+	}
+	a++;
+	return (_strlen_recursion(a) + 1);
+}	
