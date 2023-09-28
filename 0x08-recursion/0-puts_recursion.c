2a44f512c8ea72106767c92eb1dfea9b8a4790de
solojobbs <solojobbs@gmail.com>
2023-09-28 08:26:03 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion: function that prints a string, followed by a new line.
+ * _puts_recursion - function that prints a string, followed by a new line.
  *
  * @x: the string to be printed
  *
