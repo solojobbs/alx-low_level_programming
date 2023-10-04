680db08d94251190ab9a44117874f43380d23c83
solojobbs <solojobbs@gmail.com>
2023-10-04 08:06:20 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: array size
+ * @argv: array
+ *
+ * Return: 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y;
+
+	if(argc != 3)
+	{
+		printf("Error\n");
+
+		return (0);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+
+	printf("%d\n", x * y);
+
+	return (0);
+}
