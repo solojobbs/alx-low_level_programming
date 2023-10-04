def79a64790ac71a63211eefb6410f989d682e2b
solojobbs <solojobbs@gmail.com>
2023-10-04 03:35:06 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program's name
+ *
+ * @argc: argument array size
+ * @argv: argument array
+ *
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
